054eeb1bd853721f787e35df5397c9a8d9b7f2ce
Villegas <armando.villegas@mssm.edu>
2022-07-24 16:42:34 -0400

    2022 data

--- slot_process_singleday.R
+++ slot_process_singleday.R
@@ -157,8 +157,8 @@ con <- dbConnect(odbc(), Driver = "Oracle",
                  PWD = "villea04123$"
 )
 
-slot_date_1 <- "2021-07-01"
-slot_date_2 <- "2021-12-31"
+slot_date_1 <- "2022-01-01"
+slot_date_2 <- Sys.Date() - 2
 
 slot_sql <- paste0("SELECT DEPARTMENT_NAME,PROVIDER_NAME,
 	       SLOT_BEGIN_TIME,NUM_APTS_SCHEDULED,SLOT_LENGTH,
