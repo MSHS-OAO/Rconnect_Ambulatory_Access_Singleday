c53254e3e2b46eca86182e43b6540c7cd2beb480
Villegas <armando.villegas@mssm.edu>
2022-07-24 11:56:42 -0400

    yesterays data pull

--- process_amb_singleday.R
+++ process_amb_singleday.R
@@ -235,8 +235,8 @@ con <- dbConnect(odbc(), Driver = "Oracle",
 #                       UID    = "villea04",
 #                       PWD    = "villea04123$")
 
-access_date_1 <- Sys.Date() - 2
-access_date_2 <- Sys.Date() - 2
+access_date_1 <- Sys.Date() - 1
+access_date_2 <- Sys.Date() - 1
 
 print(access_date_1)
 print(access_date_2)
