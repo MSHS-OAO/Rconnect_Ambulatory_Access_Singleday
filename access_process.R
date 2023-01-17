27747f0ca3944f2247c1fda750b560607a80e829
skweon2 <48689530+skweon2@users.noreply.github.com>
2023-01-17 10:33:00 -0500

    Update access_process.R

--- access_process.R
+++ access_process.R
@@ -236,9 +236,9 @@ con <- dbConnect(odbc(), Driver = "Oracle",
 #access_date_1 <- Sys.Date()-1
 #access_date_2 <- Sys.Date()-1
 
-access_date_1 <- "2022-01-01"
-access_date_2 <- "2023-01-14"
-#access_date_2 <- Sys.Date()-1
+access_date_1 <- "2021-01-01"
+#access_date_2 <- "2023-01-14"
+access_date_2 <- Sys.Date()-1
 
 access_sql <- paste0("SELECT DEP_RPT_GRP_SEVENTEEN,DEPT_SPECIALTY_NAME,DEPARTMENT_NAME,PROV_NAME_WID,DEPARTMENT_ID,REFERRING_PROV_NAME_WID,
                      MRN,PAT_NAME,ZIP_CODE,BIRTH_DATE,FINCLASS,
