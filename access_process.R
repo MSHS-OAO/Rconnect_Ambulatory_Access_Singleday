40361d648d0141e12fa538b71807753228946ca3
skweon2 <48689530+skweon2@users.noreply.github.com>
2024-01-04 15:53:56 -0500

    Update access_process.R

--- access_process.R
+++ access_process.R
@@ -253,7 +253,7 @@ con <- dbConnect(odbc(),
 #access_date_1 <- Sys.Date()-1
 #access_date_2 <- Sys.Date()-1
 
-access_date_1 <- "2022-01-01"
+access_date_1 <- "2023-01-01"
 #access_date_2 <- "2021-12-31"
 access_date_2 <- Sys.Date()-1
 
